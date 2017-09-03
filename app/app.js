fd4352a4cca88cbf5375518e5bc03073d91daa10
fortunatopt <patrick_fortunato@yahoo.com>
2017-09-03 08:37:08 -0700

    Just a little cleanup

--- app/app.js
+++ app/app.js
@@ -1,28 +1,3 @@
-//'use strict';
-
-//angular.module("phpApp", ['ngSanitize', 'ngRoute']);
-
-
-
-
-
-
 'use strict';
 
-angular.module("phpApp", [
-    'ngSanitize',
-    'ngRoute']).run(["$rootScope", "$templateCache", "$window", "$location", "$routeParams", function ($rootScope, $templateCache, $window, $location, $routeParams) {
-
-        $rootScope.$on('$routeChangeStart', function () {
-            //console.log('$routeChangeStart');
-            $templateCache.removeAll();
-
-        });
-
-        $rootScope.$on('$viewContentLoaded', function() {
-            //console.log('$viewContentLoaded');
-            $templateCache.removeAll();
-        
-        });
-
-    }]);
+angular.module("phpApp", ['ngSanitize', 'ngRoute']);
--- app/modules/data/dataCtrl.js
+++ app/modules/data/dataCtrl.js
@@ -1,6 +1,6 @@
 angular.module('phpApp').controller("dataCtrl", function ($scope, $http){
 
-    $http.get('api/people.php', { headers: { 'Cache-Control' : 'no-cache' } }).success(function(data) {
+    $http.get('api/people.php').success(function(data) {
         $scope.people = data;
     });
 
