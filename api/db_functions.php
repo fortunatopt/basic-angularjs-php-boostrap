4883d9d8b144c33104e3c6112ec800c344db3b72
fortunatopt <patrick_fortunato@yahoo.com>
2017-09-04 08:21:04 -0700

    Added a randomizer for the names in the SQL INSERT

--- api/db_functions.php
+++ api/db_functions.php
@@ -3,6 +3,9 @@
 error_reporting(0);
 mysqli_report(MYSQLI_REPORT_STRICT);
 
+// This is a simple function to SELECT data from a MySQL database
+// It takes the query, server, username, password, and the database
+// to to run the query against
 function read($sql, $servername, $username, $password, $dbname) {
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,23 +13,26 @@ function read($sql, $servername, $username, $password, $dbname) {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
+    // Create array for return
     $rows = array();
-
+    // Run query
     $result = $conn->query($sql);
-
+    // If data, then populate array
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
             $rows[] = $row;
         }
     }
-
+    // Close connection
     $conn->close();
-
+    // Return JSON array
     return json_encode($rows);
 }
 
+// This is a simple function execute a query against a MySQL database
+// It takes the query, server, username, password, and the database
+// to to run the query against
 function execute($sql, $servername, $username, $password, $dbname) {
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -34,34 +40,33 @@ function execute($sql, $servername, $username, $password, $dbname) {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
-
+    // Run query
     if ($conn->query($sql) === TRUE) {
         return "Query executed successfully";
     } else {
         return "Error: ".$sql."<br>".$conn->error;
     }
-
+    // Close connection
     $conn->close();
 }
 
-
-
-function create($sql, $servername, $username, $password) {
-    
+// This is a simple function create a database on a MySQL server
+// It takes the query, server, username, password to to run the
+// query against
+function create($sql, $servername, $username, $password) {    
     // Create connection
     $conn = new mysqli($servername, $username, $password);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    } 
-    
-    // Create database
+    }
+    // Run query
     if ($conn->query($sql) === TRUE) {
         return "Database created successfully";
     } else {
         return "Error creating database: " . $conn->error;
     }
-    
+    // Close connection
     $conn->close();
 
 }
--- app/modules/index/indexCtrl.js
+++ app/modules/index/indexCtrl.js
@@ -1,5 +1,3 @@
 angular.module('phpApp').controller("indexCtrl", function ($scope, $http){
-
-    $scope.data = "This is the main page. From here you can use the menus above to do various things such as View data, setup the app, and remove the app.";
     
 });
